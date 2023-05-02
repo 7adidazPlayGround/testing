0195302d63007e00aba5805802e6ddaaf60f758c
Abdallah <abdallah31009448@f-eng.tanta.edu.eg>
2023-05-02 21:07:38 +0300

    json parser

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var dexter = require('morgan');
 const app = express();
 const cors = require('cors');
 
-
-
 let notes = [
   {
     id: 1,
@@ -24,6 +22,7 @@ let notes = [
 ]
 
 
+app.use(express.json());
 app.use(dexter('tiny'));
 app.use(cors());
 app.use(express.static('build'));
